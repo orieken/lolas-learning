f5f6b3439f62261c77ae0ea4f8f4cf1a921e5358
Oscar Rieken <oriekenjr@gmail.com>
2026-04-22 05:27:47 -0500

    :green_heart: (shell) fixed config path

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { withZephyr } from 'vite-plugin-zephyr';
 import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
 
